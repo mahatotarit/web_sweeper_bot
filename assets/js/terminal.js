0018347788d5fcc3bac357cc0d0476ab5fdc1088
0xtarit <0xtarit@gmail.com>
2024-06-01 12:02:46 +0530

    30% completed

--- assets/js/terminal.js
+++ assets/js/terminal.js
@@ -1,49 +1,94 @@
 let terminal_name = 'sweeperbot@terminal:~$';
+let bot_status = false;
 
 document.addEventListener('DOMContentLoaded', function () {
-  let terminal_first_time_start = true;
 
   const inputElement = document.getElementById('terminal-input');
   const outputElement = document.getElementById('terminal-output');
 
   inputElement.addEventListener('keydown', function (event) {
-    commondEnter(event);
+    commondEnter(true,event, inputElement.value);
   });
 
-  function commondEnter(event) {
+  function commondEnter(key_or_text,event,commond) {
+
+    let text_commond = commond.trim();
+
+    if(bot_status){
+
+      if(event.key === 'Enter' || !key_or_text){
+
+        if (text_commond == 'start'){
+          handleCommand(text_commond);
+        }else if(text_commond == 'stop'){
+          handleCommand(text_commond);
+        }else if(text_commond == 'clear'){
+          handleCommand(text_commond);
+        }
+
+        inputElement.value = '';
+      }
+      
+      return;
+    }
+
+    if(!key_or_text){
+      outputElement.innerHTML += `<div><span class="prompt">${terminal_name}</span> ${text_commond}</div>`;
+      inputElement.value = '';
+      handleCommand(text_commond);
+      return;
+    }
+    
     if (event.key === 'Enter') {
       const command = inputElement.value.trim();
-
       if (command == '' || command == undefined || command == null) {
         return;
       }
-
-      if (terminal_first_time_start) {
-        outputElement.innerHTML = `<div><span class="prompt">${terminal_name}</span> ${command}</div>`;
-        terminal_first_time_start = false;
-      } else {
-        outputElement.innerHTML += `<div><span class="prompt">${terminal_name}</span> ${command}</div>`;
-      }
-
+      outputElement.innerHTML += `<div><span class="prompt">${terminal_name}</span> ${command}</div>`;
       inputElement.value = '';
-      handleCommand(command, false);
+      handleCommand(command);
+      return;
     }
+
   }
 
-  function handleCommand(command, commond_check) {
+  function handleCommand(command) {
     let output;
+    let status_message = 'Sweeper Bot is currently stopped.';
+
+    if (bot_status) {
+      status_message = 'Sweeper Bot is currently running.';
+      if (command == 'stop') {
+        stop_bot();
+        outputElement.innerHTML += `<div><span class="prompt">${terminal_name}</span> ${command}</div>`;
+        inputElement.value = '';
+        output = 'Sweeper Bot stopped.';
+      } else if (command == 'start') {
+        output = 'Sweeper Bot is already started.';
+      }else if(command == 'clear'){
+        inputElement.value = '';
+        outputElement.innerHTML = ``;
+        return;
+      }
+
+      outputElement.innerHTML += `<div>&nbsp; ${output}</div>`;
+      outputElement.scrollTop = outputElement.scrollHeight;
+      return;
+    }
+
     switch (command.trim()) {
       case 'help':
         output = 'Available commands: help, start, stop, status, clear';
         break;
       case 'start':
+        start_bot();
         output = 'Sweeper Bot started.';
         break;
       case 'stop':
         output = 'Sweeper Bot stopped.';
         break;
       case 'status':
-        output = 'Sweeper Bot is currently idle.';
+        output = status_message;
         break;
       case 'clear':
         outputElement.innerHTML = '';
@@ -52,25 +97,16 @@ document.addEventListener('DOMContentLoaded', function () {
         output = `command not found: ${command}`;
     }
 
-    if (commond_check) {
-      if (terminal_first_time_start) {
-        outputElement.innerHTML = `<div><span class="prompt">${terminal_name}</span> ${command}</div>`;
-      } else {
-        outputElement.innerHTML += `<div><span class="prompt">${terminal_name}</span> ${command}</div>`;
-      }
-      terminal_first_time_start = false;
-    }
-
     outputElement.innerHTML += `<div>&nbsp; ${output}</div>`;
     outputElement.scrollTop = outputElement.scrollHeight;
   }
 
   document.getElementById('start-btn').addEventListener('click', function () {
-    handleCommand('start', true);
+    commondEnter(false, 'start', 'start');
   });
 
   document.getElementById('stop-btn').addEventListener('click', function () {
-    handleCommand('stop', true);
+    commondEnter(false, 'stop', 'stop');
   });
 
   // =========================== modal ==========================
@@ -97,8 +133,19 @@ document.addEventListener('DOMContentLoaded', function () {
     const formData = new FormData(document.getElementById('settings-form'));
     const settingsData = {};
     formData.forEach((value, key) => {settingsData[key] = value;});
-    console.log('Settings Saved:', settingsData);
     modal.style.display = 'none';
+    alert('Bot Details Saved');
   });
+
+
+
+  // =========================== bot function  ============================
+  function stop_bot() {
+    bot_status = false;
+  }
+  async function start_bot(){
+    bot_status = true;
+  }
+
 
 });
